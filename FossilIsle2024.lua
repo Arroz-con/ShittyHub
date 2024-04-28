8ca2b07a8789ea91d0c76d2db171d948bddf736f
Arroz-con <lolurbaka@gmail.com>
2024-04-28 15:08:07 -0700

    Update FossilIsle2024.lua

--- FossilIsle2024.lua
+++ FossilIsle2024.lua
@@ -4,10 +4,10 @@ local Bypass = require(ReplicatedStorage:WaitForChild("Fsys")).load
 local VI = game:GetService("VirtualInputManager")
 local ExposedBlocks = game:GetService("Workspace").Interiors.FossilIsleInterior.ExposedBlocks
 
-local function findButton()
+local function findButton(onOrOff)
     for i, v in Player.PlayerGui.ExtraButtonsApp.Frame:GetDescendants() do
         if v.Name == "TextLabel" then
-            if v.Text == "Auto Mine: Off" then
+            if v.Text == onOrOff then
                 task.wait()
                 VI:SendMouseButtonEvent(v.Parent.Parent.AbsolutePosition.X + 60, v.Parent.Parent.AbsolutePosition.Y + 60, 0, true, game, 1)
                 task.wait()
@@ -64,16 +64,19 @@ local function waitingForPickaxe(pickaxeName)
     if not Player.Character.FossilMiningPickaxe.ModelHandle[pickaxeName] then
         local count = 0
         repeat
+            findButton("Auto Mine: On")
             pickupPickaxe()
             count += 1
             print("Waiting for Pickaxe")
             task.wait(60)
         until Player.Character.FossilMiningPickaxe.ModelHandle[pickaxeName] or count == 420
+        
+        findButton("Auto Mine: Off")
     end
 end
 
 task.wait(3)
-findButton()
+findButton("Auto Mine: Off")
 
 mineBlock("Dirt")
 print("finished mining dirt")
