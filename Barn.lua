628a9b1bea57961b87c66b6f470fc24bc9445b82
Arroz-con <lolurbaka@gmail.com>
2024-06-21 21:11:16 -0700

    Update Barn.lua

--- Barn.lua
+++ Barn.lua
@@ -85,20 +85,20 @@ Player.PlayerGui.MinigameInGameApp:GetPropertyChangedSignal("Enabled"):Connect(f
                             print("doing hay task")
                             pickUpFoodForHorse("attempt_interact_with_hay_pile")
                             task.wait()
-                            feedHorse(area, 1)
+                            feedHorse(tonumber(area), 1)
 
                         elseif needs:FindFirstChild("CarrotsTemplate") then
                             print("doing carrot task")
                             pickUpFoodForHorse("attempt_interact_with_carrots_pile")
                             task.wait()
-                            feedHorse(area, 1)
+                            feedHorse(tonumber(area), 1)
 
                         elseif needs:FindFirstChild("WaterTemplate") then
                             print("doing water task")
                             pickUpFoodForHorse("attempt_interact_with_faucet") -- turns on faucet to fill bucket
                             task.wait(1)
                             pickUpBucket(1)
-                            feedHorse(area, 2)
+                            feedHorse(tonumber(area), 2)
                         end
                     end
                     task.wait(1)
