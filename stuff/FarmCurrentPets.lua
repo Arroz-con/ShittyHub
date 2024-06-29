8537be33677027956cfbf8ac050ff5e80c73ec21
Arroz-con <lolurbaka@gmail.com>
2024-06-28 23:17:48 -0700

    Update FarmCurrentPets.lua

--- stuff/FarmCurrentPets.lua
+++ stuff/FarmCurrentPets.lua
@@ -67,7 +67,7 @@ getgenv().SETTINGS = {
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Arroz-con/ShittyHub/main/Adoptme_Script"))()
 
----[[
+--[[
 local function buyPet(petNameId: string, howManyToBuy: number)
     for i = 1, howManyToBuy do
         local hasMoney = game.ReplicatedStorage.API["ShopAPI/BuyItem"]:InvokeServer("pets", petNameId, {})
