69dcd3db96bf5023c38479601b99e6d07299b168
Arroz-con <lolurbaka@gmail.com>
2024-08-29 16:28:28 -0700

    Update ClipboardPetsInfo.lua

--- stuff/ClipboardPetsInfo.lua
+++ stuff/ClipboardPetsInfo.lua
@@ -12,6 +12,15 @@ local neonPets = {}
 local normalPets = {}
 local petList = ""
 
+local petsTable = {}
+local petAccessoriesTable = {}
+local strollersTable = {}
+local foodTable = {}
+local transportTable = {}
+local toysTable = {}
+local giftsTable = {}
+local allInventory = ""
+
 
 local function getPetInfoMega(title: string)
     for _, v in Bypass("ClientData").get_data()[Player.Name].inventory.pets do
@@ -62,6 +71,101 @@ function Clipboard:CopyPetInfo()
     petList = ""
     task.wait()
     debounce = false
+end
+
+--[[ Copy all inventory information ]]
+local function getInventoryInfo(tab, tablePassOn)
+    for _, v in pairs(Bypass("ClientData").get_data()[Player.Name].inventory[tab]) do
+        if v.id == "practice_dog" then continue end
+        tablePassOn[v.id] = (tablePassOn[v.id] or 0) + 1
+    end
+end
+
+local function getTable(inventoryPassOn, tablePassOn, namePassOn)
+    for i, v in tablePassOn do
+        for _, v2 in InventoryDB[inventoryPassOn] do
+            if i == tostring(v2.id) then
+                allInventory = allInventory.."["..namePassOn.."] "..v2.name.." x"..v.."\n"
+            end
+        end
+    end
+end
+
+local function getAgeupPotionInfo()
+    local count = 0
+    for _, v in pairs(Bypass("ClientData").get_data()[Player.Name].inventory.food) do
+        if v.id == "pet_age_potion" then
+            count += 1
+        end
+    end
+    return count
+end
+
+-- local function getTokenInfo()
+--     local count = 0
+--     for _, v in pairs(Bypass("ClientData").get_data()[Player.Name].inventory.toys) do
+--         if v.id == "sunshine_2024_sunshine_token" then
+--             count += 1
+--         end
+--     end
+--     return count
+-- end
+
+local function addComma(amount)
+    local formatted = amount
+    local k
+    while true do
+        formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+        if k == 0 then
+            break
+        end
+    end
+    return formatted
+end
+
+local function getBucksInfo()
+    local potions = getAgeupPotionInfo()
+    -- local tokens = getTokenInfo() or 0
+    local potionAmount = potions * 0.04
+
+    -- local ponyPassLevel = Bypass("ClientData").get_data()[Player.Name].battle_pass_manager.show_horse.rewards_claimed
+    -- local tickets = Bypass("ClientData").get_data()[Player.Name].tickets_2024
+    local bucks = Bypass("ClientData").get_data()[Player.Name].money or 0
+    local shards = Bypass("ClientData").get_data()[Player.Name].galactic_shards_2024 or 0
+    allInventory = allInventory..`{potions} Age-up Potions + {addComma(bucks)} Bucks + {shards} Galactic Shards | Adopt me\n`
+
+    -- local cash = tonumber(bucks) * 0.000037
+
+    -- local currency = string.gsub(Player.PlayerGui.AltCurrencyIndicatorApp.CurrencyIndicator.Container.Amount.Text, ",", "")
+    -- local eventCurrency = tonumber(currency) * 0.0000035714  -- $0.05 each box at 14,000 currency per box
+    local formatNumber = string.format("%.2f", (potionAmount))
+
+    allInventory = allInventory..`sell for ${tostring(formatNumber)}  {Player.Name}\n\n`
+end
+    
+
+
+function Clipboard:CopyAllInventory()
+    getInventoryInfo("pets", petsTable)
+    getInventoryInfo("pet_accessories", petAccessoriesTable)
+    getInventoryInfo("strollers", strollersTable)
+    getInventoryInfo("food", foodTable)
+    getInventoryInfo("transport", transportTable)
+    getInventoryInfo("toys", toysTable)
+    getInventoryInfo("gifts", giftsTable)
+
+    getBucksInfo()
+    getTable("pets", petsTable, "PET")
+    getTable("pet_accessories", petAccessoriesTable, "PET_ACCESSORIE")
+    getTable("strollers", strollersTable, "STROLLER")
+    getTable("food", foodTable, "FOOD")
+    getTable("transport", transportTable, "TRANSPORT")
+    getTable("toys", toysTable, "TOY")
+    getTable("gifts", giftsTable, "GIFT")
+
+
+    setclipboard(allInventory)
+    allInventory = ""
 end
 
 return Clipboard
