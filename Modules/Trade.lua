4cb7a074309988ef04bea12a3a1b6f4ee9cc51d4
Arroz-con <lolurbaka@gmail.com>
2024-09-03 18:38:39 -0700

    Create Trade.lua

---
+++ Modules/Trade.lua
@@ -0,0 +1,52 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+
+local Bypass = require(ReplicatedStorage:WaitForChild("Fsys")).load
+
+local Trade = {}
+
+local function AcceptNegotiationAndConfirm(toggleName: string)
+    if Bypass("ClientData").get_data()[Player.Name].in_active_trade then
+        if #Bypass("ClientData").get_data()[Player.Name].trade.sender_offer.items == 0 then
+            getgenv()[toggleName]:Set(false)
+            ReplicatedStorage.API:FindFirstChild("TradeAPI/DeclineTrade"):FireServer()
+            return
+        end
+    end
+    ReplicatedStorage.API:FindFirstChild("TradeAPI/AcceptNegotiation"):FireServer()
+    task.wait(3)
+    ReplicatedStorage.API:FindFirstChild("TradeAPI/ConfirmTrade"):FireServer()
+end
+
+
+function Trade:sendTradeRequest(selectedPlayer: Instance)
+    if not Player.PlayerGui.TradeApp.Frame.Visible then
+        repeat
+            ReplicatedStorage.API:FindFirstChild("TradeAPI/SendTradeRequest"):FireServer(selectedPlayer)
+            task.wait(10)
+        until Player.PlayerGui.TradeApp.Frame.Visible
+    end
+end
+
+
+function Trade:TradeAllNeons(version: string)
+    while getgenv().auto_trade_all_neons do
+        pcall(function()
+            for _, pet in Bypass("ClientData").get_data()[Player.Name].inventory.pets do
+                if pet.properties[version] then
+                    ReplicatedStorage.API["TradeAPI/AddItemToOffer"]:FireServer(pet.unique)
+
+                    if #Bypass("ClientData").get_data()[Player.Name].trade.sender_offer.items >= 18 then
+                        break
+                    end
+                    task.wait(0.1)
+                end
+            end
+
+            AcceptNegotiationAndConfirm("TradeAllNeons")
+        end)
+    end
+end
+
+return Trade
