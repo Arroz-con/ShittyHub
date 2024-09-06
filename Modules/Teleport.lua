823eede01067279d2d3fb7090dd56d8b0281d2fd
Arroz-con <lolurbaka@gmail.com>
2024-09-06 13:53:11 -0700

    Update Teleport.lua

--- Modules/Teleport.lua
+++ Modules/Teleport.lua
@@ -35,10 +35,10 @@ end
 
 local function removeParts()
     local MainMap = workspace.Interiors:WaitForChild(tostring(workspace.Interiors:FindFirstChildWhichIsA("Model")))
-    if not MainMap then return end
+    if not MainMap then print("not in mainmap") return end
 
     local Static = MainMap:FindFirstChild("StaticMap")
-    if not Static then return end
+    if not Static then print("didnt find static") return end
 
     workspace:FindFirstChildWhichIsA("Terrain"):Clear()
     workspace:FindFirstChild("StaticMap"):FindFirstChild("Balloon"):Destroy()
