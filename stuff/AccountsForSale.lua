3cfaabeb5c04bca5cc9c3146dd34f3840441a985
Arroz-con <lolurbaka@gmail.com>
2024-09-10 13:58:24 -0700

    rfth

--- stuff/AccountsForSale.lua
+++ stuff/AccountsForSale.lua
@@ -1,52 +1,44 @@
 getgenv().SETTINGS = {
-    PET_TO_BUY = "garden_2024_egg", -- add pet or egg to buy when bot has no pets/egg left to level
+	PET_TO_BUY = "garden_2024_egg", -- add pet or egg to buy when bot has no pets/egg left to level
 
-    FOCUS_FARM_AGE_POTION = true,  -- if true, this will only farm 1 pet an keep aging to get aging potions
+	FOCUS_FARM_AGE_POTION = true, -- if true, this will only farm 1 pet an keep aging to get aging potions
 
-    CELESTIAL_SUMMER_EVENT = true,
-    
-    ENABLE_AUTO_FARM = true,
-    SET_FPS = 1,
-    PET_NEON_PRIORITY = false,
-    PET_AUTO_FUSION = false,
+	CELESTIAL_SUMMER_EVENT = true,
 
-    ENABLE_TRADE_COLLECTOR = true,
+	ENABLE_AUTO_FARM = true,
+	SET_FPS = 1,
+	PET_NEON_PRIORITY = false,
+	PET_AUTO_FUSION = false,
+
+	ENABLE_TRADE_COLLECTOR = true,
 	TRADE_ONLY_LUMINOUS_MEGA = false,
-    TRADE_COLLECTOR_NAME = "donutboy_92", -- your account username (case sensitive) that will collect the pets.
-    TRADE_LIST = {
-		PET_WEAR_TABLE = {
+	TRADE_COLLECTOR_NAME = "donutboy_92", -- your account username (case sensitive) that will collect the pets.
+	TRADE_LIST = {
+		PET_WEAR_TABLE = {},
 
-        },
+		VEHICLES_TABLE = {},
 
-        VEHICLES_TABLE = {
+		GIFTS_TABLE = {
+			"halloween_2023_scarecrow_box",
+			"halloween_2022_wolf_box",
+			"summerfest_2023_hermit_crab_box",
+			"winter_2021_walrus_box",
+			"winter_2022_pony_box",
+			"lny_2023_moon_bear_box",
+			"springfest_2023_duckling_box",
+			"lunar_2024_special_lunar_new_year_gift_box",
+			"easter_2024_eggy_box",
+		},
 
-        },
-	
-        GIFTS_TABLE = {
-            "halloween_2023_scarecrow_box",
-            "halloween_2022_wolf_box",
-            "summerfest_2023_hermit_crab_box",
-            "winter_2021_walrus_box",
-            "winter_2022_pony_box",
-            "lny_2023_moon_bear_box",
-            "springfest_2023_duckling_box",
-	        "lunar_2024_special_lunar_new_year_gift_box",
-            "easter_2024_eggy_box"
-        },
-    
-        TOYS_TABLE = {
-		
-        },
-		
-		FOOD_TABLE = {
+		TOYS_TABLE = {},
 
-        },
+		FOOD_TABLE = {},
 
-        -- the pets in here will be traded no matter what age they are
-        PETS_TABLE = { 
-            "lures_2023_blazing_lion",
-            "golden_egg",
-            "diamond_egg",
+		-- the pets in here will be traded no matter what age they are
+		PETS_TABLE = {
+			"lures_2023_blazing_lion",
+			"golden_egg",
+			"diamond_egg",
 			"lures_2023_magma_moose",
 			"lures_2023_flaming_zebra",
 			"lures_2023_toasty_red_panda",
@@ -56,53 +48,51 @@ getgenv().SETTINGS = {
 			"fire_dimension_2024_flaming_fox",
 			"fire_dimension_2024_burning_bunny",
 			"fire_dimension_2024_wildfire_hawk",
-            "summerfest_2024_kid_goat",
-            "summerfest_2024_cow_calf",
-            "summerfest_2024_orange_betta_fish",
-            "summerfest_2024_pink_betta_fish",
-            "summerfest_2024_blue_betta_fish",
-            "summerfest_2024_show_pony",
-            "summerfest_2024_punk_pony",
-            "summerfest_2024_pretty_pony",
-            "summerfest_2024_majestic_pony",
-            "summerfest_2024_corn_doggo",
-            "summerfest_2024_bull",
-            "garden_2024_rosy_maple_moth",
-            "garden_2024_mushroom_friend"
-        },
-    },
+			"summerfest_2024_kid_goat",
+			"summerfest_2024_cow_calf",
+			"summerfest_2024_orange_betta_fish",
+			"summerfest_2024_pink_betta_fish",
+			"summerfest_2024_blue_betta_fish",
+			"summerfest_2024_show_pony",
+			"summerfest_2024_punk_pony",
+			"summerfest_2024_pretty_pony",
+			"summerfest_2024_majestic_pony",
+			"summerfest_2024_corn_doggo",
+			"summerfest_2024_bull",
+			"garden_2024_rosy_maple_moth",
+			"garden_2024_mushroom_friend",
+		},
+	},
 
-    HATCH_EGG_PRIORITY = false,
-    HATCH_EGG_PRIORITY_NAMES = {"garden_2024_egg"},
-    
-    PET_ONLY_PRIORITY = true,
-    PET_ONLY_PRIORITY_NAMES = {
-        "starter_egg",
-        "dog",
-        "cat"
-    },
+	HATCH_EGG_PRIORITY = false,
+	HATCH_EGG_PRIORITY_NAMES = { "garden_2024_egg" },
+
+	PET_ONLY_PRIORITY = true,
+	PET_ONLY_PRIORITY_NAMES = {
+		"starter_egg",
+		"dog",
+		"cat",
+	},
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Arroz-con/ShittyHub/main/Adoptme_Script"))()
 
---[[
-local petToBuyId = "celestial_2024_glormy_leo"
-local howMany = 8
-
+---[[
 local function buyPet(petNameId: string, howManyToBuy: number)
-    for _ = 1, howManyToBuy do
-        local hasMoney = game.ReplicatedStorage.API["ShopAPI/BuyItem"]:InvokeServer("pets", petNameId, {})
-        if hasMoney == "too little money" then
-            return
-        end
-        task.wait(.1)
-    end
+	for _ = 1, howManyToBuy do
+		local hasMoney = game.ReplicatedStorage.API["ShopAPI/BuyItem"]:InvokeServer("pets", petNameId, {})
+		if hasMoney == "too little money" then
+			return
+		end
+		task.wait(0.1)
+	end
 end
 
-buyPet(petToBuyId, howMany)
+buyPet("celestial_2024_glormy_hound", 2)
+buyPet("celestial_2024_glormy_leo", 2)
 --]]
 
----[[
+--[[
 local Bypass = require(game.ReplicatedStorage:WaitForChild("Fsys", 600)).load
 local Player = game:GetService("Players").LocalPlayer
 local selectedItem = "celestial_2024_moonlight_moth"
--- stuff/FarmCurrentPets.lua
+++ stuff/FarmCurrentPets.lua
@@ -1,98 +1,96 @@
 getgenv().SETTINGS = {
-    PET_TO_BUY = "garden_2024_egg", -- add pet or egg to buy when bot has no pets/egg left to level
+	PET_TO_BUY = "garden_2024_egg", -- add pet or egg to buy when bot has no pets/egg left to level
 
-    CELESTIAL_SUMMER_EVENT = true,
-    
-    FOCUS_FARM_AGE_POTION = false,  -- if true, this will only farm 1 pet an keep aging to get aging potions
+	CELESTIAL_SUMMER_EVENT = true,
 
-    DO_SUNSHINE_EVENT = true,
-    ENABLE_AUTO_FARM = true,
-    SET_FPS = 1,
-    PET_NEON_PRIORITY = true,
-    PET_AUTO_FUSION = true,
+	FOCUS_FARM_AGE_POTION = false, -- if true, this will only farm 1 pet an keep aging to get aging potions
 
-    ENABLE_TRADE_COLLECTOR = true,
-    TRADE_ONLY_LUMINOUS_MEGA = true,
-    TRADE_COLLECTOR_NAME = "donutboy_92", -- your account username (case sensitive) that will collect the pets.
-    TRADE_LIST = {
-        PET_WEAR_TABLE = {
+	DO_SUNSHINE_EVENT = true,
+	ENABLE_AUTO_FARM = true,
+	SET_FPS = 1,
+	PET_NEON_PRIORITY = true,
+	PET_AUTO_FUSION = true,
 
-        },
+	ENABLE_TRADE_COLLECTOR = true,
+	TRADE_ONLY_LUMINOUS_MEGA = true,
+	TRADE_COLLECTOR_NAME = "donutboy_92", -- your account username (case sensitive) that will collect the pets.
+	TRADE_LIST = {
+		PET_WEAR_TABLE = {},
 
-        VEHICLES_TABLE = {
-            -- "lunar_2024_dragonster",
-        },
-        
-        GIFTS_TABLE = {
-            "rgb_reward_box",
-            "halloween_2023_scarecrow_box",
-            "halloween_2022_wolf_box",
-            "summerfest_2023_hermit_crab_box",
-            "winter_2021_walrus_box",
-            "winter_2022_pony_box",
-            "lny_2023_moon_bear_box",
-            "springfest_2023_duckling_box",
-            "lunar_2024_silk_bag",
-            "lunar_2024_special_lunar_new_year_gift_box",
-        },
+		VEHICLES_TABLE = {
+			-- "lunar_2024_dragonster",
+		},
 
-        FOOD_TABLE = {
-            -- "cure_all_potion",
-        },
-    
-        TOYS_TABLE = {
-            -- "paint_2023_colored_hair_spray_sealer",
-        },
+		GIFTS_TABLE = {
+			"rgb_reward_box",
+			"halloween_2023_scarecrow_box",
+			"halloween_2022_wolf_box",
+			"summerfest_2023_hermit_crab_box",
+			"winter_2021_walrus_box",
+			"winter_2022_pony_box",
+			"lny_2023_moon_bear_box",
+			"springfest_2023_duckling_box",
+			"lunar_2024_silk_bag",
+			"lunar_2024_special_lunar_new_year_gift_box",
+		},
 
-        -- the pets in here will be traded no matter what age they are
-        PETS_TABLE = {
-            "lures_2023_blazing_lion",
-            "celestial_2024_moonlight_moth",
-            -- "celestial_2024_glormy_hound",
-            -- "summerfest_2024_punk_pony",
-            -- "summerfest_2024_pretty_pony",
-            -- "summerfest_2024_majestic_pony"
-        },
-    },
+		FOOD_TABLE = {
+			-- "cure_all_potion",
+		},
 
-    HATCH_EGG_PRIORITY = true,
-    HATCH_EGG_PRIORITY_NAMES = {"garden_2024_egg"},
-    
-    PET_ONLY_PRIORITY = true,
-    PET_ONLY_PRIORITY_NAMES = {
-        "celestial_2024_moonlight_moth",
-        "garden_2024_rosy_maple_moth",
-        "garden_2024_mushroom_friend",
-        -- "garden_2024_praying_mantis",
-        "garden_2024_skunk",
-        -- "garden_2024_weevil",
-        -- "garden_2024_blue_jay",
-        "garden_2024_mole",
-        -- "summerfest_2024_cow_calf",
-        "summerfest_2024_kid_goat",
-        "summerfest_2024_punk_pony",
-        "summerfest_2024_pretty_pony",
-        "summerfest_2024_majestic_pony",
-        "summerfest_2024_corn_doggo",
-        "sunshine_2024_cheetah",
-        "celestial_2024_glormy_hound",
-        "celestial_2024_glormy_leo"
-    },
+		TOYS_TABLE = {
+			-- "paint_2023_colored_hair_spray_sealer",
+		},
+
+		-- the pets in here will be traded no matter what age they are
+		PETS_TABLE = {
+			"lures_2023_blazing_lion",
+			"celestial_2024_moonlight_moth",
+			-- "celestial_2024_glormy_hound",
+			-- "summerfest_2024_punk_pony",
+			-- "summerfest_2024_pretty_pony",
+			-- "summerfest_2024_majestic_pony"
+		},
+	},
+
+	HATCH_EGG_PRIORITY = true,
+	HATCH_EGG_PRIORITY_NAMES = { "garden_2024_egg" },
+
+	PET_ONLY_PRIORITY = true,
+	PET_ONLY_PRIORITY_NAMES = {
+		"celestial_2024_moonlight_moth",
+		"garden_2024_rosy_maple_moth",
+		"garden_2024_mushroom_friend",
+		-- "garden_2024_praying_mantis",
+		"garden_2024_skunk",
+		-- "garden_2024_weevil",
+		-- "garden_2024_blue_jay",
+		"garden_2024_mole",
+		-- "summerfest_2024_cow_calf",
+		"summerfest_2024_kid_goat",
+		"summerfest_2024_punk_pony",
+		"summerfest_2024_pretty_pony",
+		"summerfest_2024_majestic_pony",
+		"summerfest_2024_corn_doggo",
+		"sunshine_2024_cheetah",
+		"celestial_2024_glormy_hound",
+		"celestial_2024_glormy_leo",
+	},
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Arroz-con/ShittyHub/main/Adoptme_Script"))()
 
 ---[[
 local function buyPet(petNameId: string, howManyToBuy: number)
-    for _ = 1, howManyToBuy do
-        local hasMoney = game.ReplicatedStorage.API["ShopAPI/BuyItem"]:InvokeServer("pets", petNameId, {})
-        if hasMoney == "too little money" then
-            return
-        end
-        task.wait(.1)
-    end
+	for _ = 1, howManyToBuy do
+		local hasMoney = game.ReplicatedStorage.API["ShopAPI/BuyItem"]:InvokeServer("pets", petNameId, {})
+		if hasMoney == "too little money" then
+			return
+		end
+		task.wait(0.1)
+	end
 end
 
-buyPet("celestial_2024_glormy_hound", 1)
-buyPet("celestial_2024_glormy_leo", 1)
+buyPet("celestial_2024_glormy_hound", 2)
+buyPet("celestial_2024_glormy_leo", 2)
 --]]
