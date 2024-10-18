797e1613c0b2f773e6005467f14f7183d5007484
Arroz-con <lolurbaka@gmail.com>
2024-10-18 09:16:10 -0700

    Update FarmCurrentPets.lua

--- stuff/FarmCurrentPets.lua
+++ stuff/FarmCurrentPets.lua
@@ -67,6 +67,9 @@ getgenv().SETTINGS = {
 
 	PET_ONLY_PRIORITY = true,
 	PET_ONLY_PRIORITY_NAMES = {
+		"halloween_2024_evil_chick",
+		"halloween_2024_ghost_chick",
+		"halloween_2024_zombie_chick",
 		"halloween_2024_scarebear",
 		"halloween_2024_dracula_parrot",
 		"halloween_2024_pumpkin_friend",
