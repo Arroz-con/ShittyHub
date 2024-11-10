0aa8b609a341103ab174bc50c0a97171d489c7a4
Arroz-con <lolurbaka@gmail.com>
2024-11-10 16:00:54 -0700

    Update Teleport.lua

--- Modules/Teleport.lua
+++ Modules/Teleport.lua
@@ -150,10 +150,11 @@ end
 
 function Teleport.CampSite()
 	Player.Character:WaitForChild("HumanoidRootPart").Anchored = true
-	local isAlreadyOnMainMap = workspace:FindFirstChild("Interiors"):FindFirstChild("center_map_plot", true)
-	if not isAlreadyOnMainMap then
-		SetLocationFunc("MainMap", "Neighborhood/MainDoor", {})
-	end
+	-- local isAlreadyOnMainMap = workspace:FindFirstChild("Interiors"):FindFirstChild("center_map_plot", true)
+	-- if not isAlreadyOnMainMap then
+	-- 	SetLocationFunc("MainMap", "Neighborhood/MainDoor", {})
+	-- end
+	SetLocationFunc("MainMap", "Neighborhood/MainDoor", {})
 	task.wait(1)
 	workspace.Interiors:WaitForChild(tostring(workspace.Interiors:FindFirstChildWhichIsA("Model")))
 	floorPart()
