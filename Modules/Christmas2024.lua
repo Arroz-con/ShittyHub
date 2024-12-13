edee97eb1308235e9f2b5beab10ead5462e4da27
Arroz-con <lolurbaka@gmail.com>
2024-12-13 12:27:52 -0700

    Update Christmas2024.lua

--- Modules/Christmas2024.lua
+++ Modules/Christmas2024.lua
@@ -4,6 +4,66 @@ local ClientData = require(ReplicatedStorage:WaitForChild("ClientModules"):WaitF
 
 local Christmas2024 = {}
 
+local function createLobby()
+    print(ReplicatedStorage.API["MinigameAPI/LobbyCreate"]:InvokeServer("frostclaws_revenge"))
+end
+
+local function startLobby()
+    ReplicatedStorage.API["MinigameAPI/LobbyStart"]:FireServer()
+end
+
+local function getMinigameId()
+    local folderName = workspace.Minigames:FindFirstChildWhichIsA("Folder")
+    if not folderName then
+        local count = 0
+        repeat
+            task.wait(1)
+            count += 1
+            folderName = workspace.Minigames:FindFirstChildWhichIsA("Folder")
+        until folderName  or count > 60
+        if count > 60 then
+            print("wouldnt get minigame id")
+            return nil
+        end
+    end
+
+    return folderName.Name:split("::")[2]
+end
+
+local function hitEnemy(name, gameId)
+    local args = {
+        [1] = "frostclaws_revenge::"..gameId,
+        [2] = "hit_enemies",
+        [3] = {
+            [1] = name
+        },
+        [4] = "sword_slash"
+    }
+
+    ReplicatedStorage.API["MinigameAPI/MessageServer"]:FireServer(unpack(args))
+end
+
+function Christmas2024.StartGame()
+    createLobby()
+    startLobby()
+
+    local minigameId = getMinigameId()
+    if not minigameId then return end
+
+    local isGameActive = true
+
+    while isGameActive do
+        for _, v in workspace.Minigames[`FrostclawsRevengeInterior::{minigameId}`]:WaitForChild("FrostclawsRevengeEnemies"):GetChildren() do
+            hitEnemy(v.Name, minigameId)
+        end
+        
+        local minigameStateFolder = workspace.StaticMap:FindFirstChild(`frostclaws_revenge::{minigameId}_minigame_state`)
+        if not minigameStateFolder then print("game over or no folder") break end
+        isGameActive = minigameStateFolder:WaitForChild("is_game_active").Value
+        task.wait(1)
+    end
+end
+
 function Christmas2024.getGingerbread()
     local GingerbreadMarkers = ReplicatedStorage.Resources.IceSkating.GingerbreadMarkers
     for _, v in GingerbreadMarkers:GetChildren() do
