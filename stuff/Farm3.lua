155a404f1d5fcbdf0edba9644250fa550ab1fb6b
Arroz-con <lolurbaka@gmail.com>
2025-01-06 00:07:26 -0700

    gver

--- stuff/Farm3.lua
+++ stuff/Farm3.lua
@@ -1,7 +1,7 @@
--- getgenv().BUY_BEFORE_FARMING = {
--- 	{NameId = "winter_2024_winter_deer_bait", MaxAmount = 6},
--- 	{NameId = "winter_2024_frostbite_cub", MaxAmount = 72}
--- }
+getgenv().BUY_BEFORE_FARMING = {
+	{NameId = "winter_2024_winter_deer_bait", MaxAmount = 6},
+	{NameId = "winter_2024_frostclaw", MaxAmount = 100}
+}
 
 -- getgenv().OPEN_ITEMS_BEFORE_FARMING = {
 -- 	"winter_2024_ice_tray"
--- stuff/FarmCurrentPets.lua
+++ stuff/FarmCurrentPets.lua
@@ -1,9 +1,9 @@
 getgenv().BUY_BEFORE_FARMING = {
-	{NameId = "winter_2024_winter_deer_bait", MaxAmount = 32},
-	{NameId = "winter_2024_ice_tray", MaxAmount = 32},
-	{NameId = "winter_2024_bauble_buddies", MaxAmount = 32},
-	{NameId = "winter_2024_great_pyrenees", MaxAmount = 64},
-	{NameId = "winter_2024_mistletroll", MaxAmount = 64},
+	{NameId = "winter_2024_bauble_buddies", MaxAmount = 80},
+	{NameId = "winter_2024_great_pyrenees", MaxAmount = 80},
+	{NameId = "winter_2024_mistletroll", MaxAmount = 160},
+	{NameId = "winter_2024_winter_deer_bait", MaxAmount = 200},
+	{NameId = "winter_2024_ice_tray", MaxAmount = 200},
 }
 
 -- getgenv().OPEN_ITEMS_BEFORE_FARMING = {
