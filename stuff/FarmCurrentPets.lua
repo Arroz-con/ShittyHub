3b4dcf18f23449d2f4d4874edab01f4bbfa7d4f0
Arroz-con <lolurbaka@gmail.com>
2025-01-19 16:41:38 -0700

    Update FarmCurrentPets.lua

--- stuff/FarmCurrentPets.lua
+++ stuff/FarmCurrentPets.lua
@@ -13,12 +13,12 @@
 
 getgenv().AGE_PETS_BEFORE_FARMING = {
 	"winter_2024_frostbite_cub",
-	"winter_2024_winter_fawn",
-	"winter_2024_winter_buck",
-	"winter_2024_great_pyrenees",
-	"winter_2024_bauble_buddies",
-	"winter_2024_mistletroll",
-	"ddlm_2024_grave_owl",
+	-- "winter_2024_winter_fawn",
+	-- "winter_2024_winter_buck",
+	-- "winter_2024_great_pyrenees",
+	-- "winter_2024_bauble_buddies",
+	-- "winter_2024_mistletroll",
+	-- "ddlm_2024_grave_owl",
 }
 
 getgenv().SETTINGS = {
@@ -28,7 +28,7 @@ getgenv().SETTINGS = {
 	},
 
 	EVENT = {
-		DO_FROSTCLAW_MINIGAME = true,
+		DO_FROSTCLAW_MINIGAME = false,
 		DO_MINIGAME = false,
 		IS_AUTO_BUY = false,
 		BUY = "halloween_2024_chick_box"
@@ -45,7 +45,7 @@ getgenv().SETTINGS = {
 
 	ENABLE_TRADE_COLLECTOR = true,
 	TRADE_ONLY_LUMINOUS_MEGA = false,
-	TRADE_COLLECTOR_NAME = {"candymine8", "MohnsGurry6277", "AnleuBlice039", "HockeDeasy246", "WopatMart", "candyx_82"},
+	TRADE_COLLECTOR_NAME = {"candymine8", "BizalTuter431"},
 	TRADE_LIST = {
 		"winter_2024_ratatoskr",
 		"winter_2024_bauble_buddies",
