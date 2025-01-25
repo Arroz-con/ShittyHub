d57eeaa5a4ac3a28d8db011c134fb41d1a0d988a
Arroz-con <lolurbaka@gmail.com>
2025-01-25 11:37:41 -0700

    Update FarmCurrentPets.lua

--- stuff/FarmCurrentPets.lua
+++ stuff/FarmCurrentPets.lua
@@ -1,27 +1,22 @@
--- getgenv().BUY_BEFORE_FARMING = {
--- 	{NameId = "winter_2024_bauble_buddies", MaxAmount = 80},
--- 	{NameId = "winter_2024_great_pyrenees", MaxAmount = 80},
--- 	{NameId = "winter_2024_mistletroll", MaxAmount = 160},
--- 	{NameId = "winter_2024_winter_deer_bait", MaxAmount = 200},
--- 	{NameId = "winter_2024_ice_tray", MaxAmount = 200},
--- 	{NameId = "winter_2024_bauble_buddies", MaxAmount = 10000}
--- }
+getgenv().BUY_BEFORE_FARMING = {
+	{NameId = "lunar_2025_blossom_snake", MaxAmount = 10}
+}
 
 -- getgenv().OPEN_ITEMS_BEFORE_FARMING = {
 -- 	"winter_2024_ice_tray"
 -- }
 
-getgenv().AGE_PETS_BEFORE_FARMING = {
-	"winter_2024_frostclaw",
-	-- "winter_2024_frostbite_cub",
-	-- "chiprac_2023_raccoon",
-	-- "winter_2024_winter_fawn",
-	-- "winter_2024_winter_buck",
-	-- "winter_2024_great_pyrenees",
-	-- "winter_2024_bauble_buddies",
-	-- "winter_2024_mistletroll",
-	-- "ddlm_2024_grave_owl",
-}
+-- getgenv().AGE_PETS_BEFORE_FARMING = {
+-- 	"winter_2024_frostclaw",
+-- 	-- "winter_2024_frostbite_cub",
+-- 	-- "chiprac_2023_raccoon",
+-- 	-- "winter_2024_winter_fawn",
+-- 	-- "winter_2024_winter_buck",
+-- 	-- "winter_2024_great_pyrenees",
+-- 	-- "winter_2024_bauble_buddies",
+-- 	-- "winter_2024_mistletroll",
+-- 	-- "ddlm_2024_grave_owl",
+-- }
 
 getgenv().SETTINGS = {
 	WEBHOOK = {
