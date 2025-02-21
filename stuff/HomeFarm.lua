4173e535b273fa2df76c3915ead9a2b8e104fc5d
Arroz-con <lolurbaka@gmail.com>
2025-02-21 09:17:09 -0700

    egerg

--- stuff/HomeFarm.lua
+++ stuff/HomeFarm.lua
@@ -1,5 +1,5 @@
 getgenv().BUY_BEFORE_FARMING = {
-	{NameId = "valentines_2025_love_bird", MaxAmount = 100}
+	{NameId = "valentines_2025_sweetheart_rat", MaxAmount = 16}
 }
 
 -- getgenv().OPEN_ITEMS_BEFORE_FARMING = {
