9944a8df418f2944f82a5c0ab62ccbb819e6604a
Arroz-con <lolurbaka@gmail.com>
2025-02-21 08:04:02 -0700

    Update HomeFarm.lua

--- stuff/HomeFarm.lua
+++ stuff/HomeFarm.lua
@@ -1,6 +1,6 @@
--- getgenv().BUY_BEFORE_FARMING = {
--- 	{NameId = "valentines_2025_love_bird", MaxAmount = 16}
--- }
+getgenv().BUY_BEFORE_FARMING = {
+	{NameId = "valentines_2025_love_bird", MaxAmount = 100}
+}
 
 -- getgenv().OPEN_ITEMS_BEFORE_FARMING = {
 -- 	"winter_2024_ice_tray"
