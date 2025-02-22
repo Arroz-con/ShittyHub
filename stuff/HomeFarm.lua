8572e5d6926e80816d9af91639a9f781a3cae662
Arroz-con <lolurbaka@gmail.com>
2025-02-22 13:31:48 -0700

    rbgrtyg

--- stuff/HomeFarm.lua
+++ stuff/HomeFarm.lua
@@ -1,14 +1,14 @@
 getgenv().BUY_BEFORE_FARMING = {
-	{NameId = "valentines_2025_sweetheart_rat", MaxAmount = 16}
+	{NameId = "valentines_2025_sweetheart_rat", MaxAmount = 32}
 }
 
 -- getgenv().OPEN_ITEMS_BEFORE_FARMING = {
 -- 	"winter_2024_ice_tray"
 -- }
 
--- getgenv().AGE_PETS_BEFORE_FARMING = {
--- 	"valentines_2025_love_bird"
--- }
+getgenv().AGE_PETS_BEFORE_FARMING = {
+	"valentines_2025_sweetheart_rat"
+}
 
 getgenv().SETTINGS = {
 	WEBHOOK = {
