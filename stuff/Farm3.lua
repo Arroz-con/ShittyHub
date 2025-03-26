85476e9d8833bd5d57004ebe8078815cc190221f
Arroz-con <lolurbaka@gmail.com>
2025-03-26 12:08:07 -0700

    Update Farm3.lua

--- stuff/Farm3.lua
+++ stuff/Farm3.lua
@@ -1,5 +1,6 @@
 getgenv().BUY_BEFORE_FARMING = {
-	{NameId = "ice_dimension_2025_shiver_cone_bait", MaxAmount = 1000}
+	{NameId = "ice_dimension_2025_shiver_cone_bait", MaxAmount = 1000},
+	{NameId = "ice_dimension_2025_subzero_popsicle_bait", MaxAmount = 1000}
 }
 
 -- getgenv().OPEN_ITEMS_BEFORE_FARMING = {
@@ -17,8 +18,7 @@ getgenv().SETTINGS = {
 	},
 
 	EVENT = {
-		DO_FROSTCLAW_MINIGAME = false,
-		DO_MINIGAME = false,
+		DO_MINIGAME = true,
 		IS_AUTO_BUY = false,
 		BUY = "halloween_2024_chick_box"
 	},
@@ -28,19 +28,20 @@ getgenv().SETTINGS = {
 	FOCUS_FARM_AGE_POTION = false, -- if true, this will only farm 1 pet an keep aging to get aging potions
 
 	ENABLE_AUTO_FARM = true,
-	SET_FPS = 3,
+	SET_FPS = 6,
 	PET_NEON_PRIORITY = true,
 	PET_AUTO_FUSION = true,
 
 	ENABLE_TRADE_COLLECTOR = true,
 	TRADE_ONLY_LUMINOUS_MEGA = false,
-	TRADE_COLLECTOR_NAME = {"candymine8", "Tiredbloxypets"},
+	TRADE_COLLECTOR_NAME = {"exp_potion", "Tiredbloxypets"},
 	TRADE_LIST = {
 		"moon_2025_royal_egg",
 		-- "moon_2025_dimension_drifter",
 		-- "moon_2025_sunglider",
 		-- "moon_2025_puptune",
 		"lures_2023_blazing_lion",
+		"ice_dimension_2025_frostbite_bear",
 	},
 
 	HATCH_EGG_PRIORITY = false,
