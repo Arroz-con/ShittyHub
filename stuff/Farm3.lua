13f8b033799726d47efa23c5c62e12cb2bccf0e7
Arroz-con <lolurbaka@gmail.com>
2025-03-27 20:04:13 -0700

    gergerg

--- stuff/Farm3.lua
+++ stuff/Farm3.lua
@@ -33,7 +33,7 @@ getgenv().SETTINGS = {
 	PET_AUTO_FUSION = true,
 
 	ENABLE_TRADE_COLLECTOR = true,
-	TRADE_ONLY_LUMINOUS_MEGA = false,
+	TRADE_ONLY_LUMINOUS_MEGA = true,
 	TRADE_COLLECTOR_NAME = {"exp_potion", "Tiredbloxypets"},
 	TRADE_LIST = {
 		"moon_2025_royal_egg",
