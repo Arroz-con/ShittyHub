e9d3c7a9b52ec91f8fe61963233da6ccb1211ba4
Arroz-con <lolurbaka@gmail.com>
2025-04-04 15:17:24 -0700

    Update FarmCurrentPets.lua

--- stuff/FarmCurrentPets.lua
+++ stuff/FarmCurrentPets.lua
@@ -1,7 +1,6 @@
--- getgenv().BUY_BEFORE_FARMING = {
--- 	{NameId = "ice_dimension_2025_subzero_popsicle_bait", MaxAmount = 1000},
--- 	{NameId = "ice_dimension_2025_shiver_cone_bait", MaxAmount = 4000},
--- }
+getgenv().BUY_BEFORE_FARMING = {
+	{NameId = "spring_2025_bakeneko", MaxAmount = 1000},
+}
 
 -- getgenv().OPEN_ITEMS_BEFORE_FARMING = {
 -- 	"winter_2024_ice_tray"
@@ -48,6 +47,7 @@ getgenv().SETTINGS = {
 
 	PET_ONLY_PRIORITY = true,
 	PET_ONLY_PRIORITY_NAMES = {
+		"spring_2025_bakeneko",
 		"ice_dimension_2025_shiver_wolf",
 		"ice_dimension_2025_subzero_scorpion",
 		"ice_dimension_2025_snowy_mammoth",
