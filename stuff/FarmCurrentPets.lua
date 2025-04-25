213cbd9c9cff1f0af33b0ec3e849418a47830971
Arroz-con <lolurbaka@gmail.com>
2025-04-25 08:36:57 -0700

    Update FarmCurrentPets.lua

--- stuff/FarmCurrentPets.lua
+++ stuff/FarmCurrentPets.lua
@@ -1,5 +1,5 @@
 getgenv().BUY_BEFORE_FARMING = {
-	{NameId = "spring_2025_kappakid", MaxAmount = 300},
+	{NameId = "spring_2025_kappakid", MaxAmount = 48},
 	{NameId = "spring_2025_mirai_moth", MaxAmount = 300},
 }
 
